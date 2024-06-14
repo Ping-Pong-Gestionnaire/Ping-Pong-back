64ab5482573759b70b59e9cf741a75f11fe9a53c
Malauryyt <110236103+Malauryyt@users.noreply.github.com>
2024-06-14 10:00:09 +0200

    suppression + getAll et getOne

--- src/controller/machine.route.js
+++ src/controller/machine.route.js
@@ -26,24 +26,23 @@ router.post("/modif", body('id'), body('nom'), body('id_poste'),async(req,res) =
 });
 router.post("/supp",body('id'),async(req,res) => {
 
-    const suppPoste =  await posteRepository.suppPoste(req.body.id);
-    console.log(suppPoste);
-    if(suppPoste === "ok"){
+    const suppMachine =  await machineRepository.suppMachine(req.body.id);
+    if(suppMachine === "ok"){
         res.status(200).end();
     }
     else{
-        res.status(400).send(suppPoste);
+        res.status(400).send(suppMachine);
     }
 });
 router.get("/getAll",async(req,res) => {
 
-    let getAll =  await posteRepository.getAll();
+    let getAll =  await machineRepository.getAll();
     res.status(200).json(getAll);
 
 });
 router.get("/getOne/:id",async(req,res) => {
 
-    let getOne=  await posteRepository.getOne(req.params.id);
+    let getOne=  await machineRepository.getOne(req.params.id);
     res.status(200).json(getOne);
 
 });
