64a6419522857e87010811e5775846e46289fb75
Malauryyt <110236103+Malauryyt@users.noreply.github.com>
2024-07-03 14:58:36 +0200

    mise en place connection db

--- src/datamodel/db.js
+++ src/datamodel/db.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-exports.sequelize= new  Sequelize(process.env.DB_HOST) // Example for postgres
+var dsn = process.env.DB_HOST
+if (dsn === undefined) {
+    const { env } = process;
+    const read_base64_json = function(varName) {
+        try {
+            return JSON.parse(Buffer.from(env[varName], "base64").toString())
+        }
+        catch (err) {
+            throw new Error(`no ${varName} environment variable`)
+        } };
+    const variables = read_base64_json('PLATFORM_VARIABLES')
+    dsn = variables["CONNECTION_STRING"]
+}
+exports.sequelize= new  Sequelize(dsn) // Example for postgres
+//exports.sequelize= new  Sequelize(process.env.DB_HOST) // Example for postgres
 
 
 
